eda5e6bbc3b68c9240a96cdae8f3a433cdddf118
Hannah Beinstein <hannah.beinstein@gmail.com>
2020-01-23 21:30:35 -0800

    nothing

---
+++ src/components/Layout.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Layout = (props) => {
+    console.log(props)
+    return (
+        <div>
+            {props.children}
+        </div>
+    )
+}
+
+export default Layout
